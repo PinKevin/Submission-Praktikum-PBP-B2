8fd4d2449360af86f6ca51d86cda9db645d7b5a9
Keisya Intan Nabila <128314097+Keisya31@users.noreply.github.com>
2024-10-01 06:15:55 +0700

    Update search_books.php
    update file

--- 24060122130063/submission-5/search_books.php
+++ 24060122130063/submission-5/search_books.php
@@ -12,11 +12,10 @@ $result = $db->query($query);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_object()) {
-        echo "<div onclick=\"showBookDetails('" . $row->isbn . "')\" style='cursor:pointer; margin-bottom: 15px;'>";
-        echo "<p><strong>Title:</strong> " . $row->title . "</p>";
-        echo "<p><strong>ISBN:</strong> " . $row->isbn . "</p>";
-        echo "<p><strong>Author:</strong> " . $row->author . "</p>";
-        echo "<p><strong>Price:</strong> $" . $row->price . "</p>";
+        echo "<br><strong>Title:</strong> " . $row->title . "<br>";
+        echo "<strong>ISBN:</strong> " . $row->isbn . "<br>";
+        echo "<strong>Author:</strong> " . $row->author . "<br>";
+        echo "<strong>Price:</strong> $" . $row->price . "<br>";
         echo "</div><hr>";
     }
 } else {
