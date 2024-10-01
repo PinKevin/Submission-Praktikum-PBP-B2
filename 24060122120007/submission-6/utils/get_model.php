9d7d86ef463045238bf5a5dd4c2893c9109bfe10
titahsahputra <titahsahputra123@gmail.com>
2024-10-01 17:55:16 +0700

    24060122120007 responsi seadanya:((((

---
+++ 24060122120007/submission-6/utils/get_model.php
@@ -0,0 +1,16 @@
+<?php
+require_once(__DIR__ . '/../lib/db_login.php');
+$brand_code = $db->real_escape_string($_GET['brand_code']);
+
+$query = "SELECT * FROM models WHERE brand_code = '$brand_code'";
+$result = $db->query($query);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<option value='{$row['model_code']}'>{$row['model_name']}</option>";
+    }
+} else {
+    echo "<option value=''>Model tidak tersedia</option>";
+}
+$result->free();
+$db->close();
