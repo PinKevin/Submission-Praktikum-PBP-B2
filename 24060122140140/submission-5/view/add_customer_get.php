b9f846ccb6c19b15caf576bd5fd233502a034340
Aly <100826322+Falydra@users.noreply.github.com>
2024-10-13 15:15:21 +0700

    init

---
+++ 24060122140140/submission-5/view/add_customer_get.php
@@ -0,0 +1,29 @@
+
+<?php 
+  require_once('../lib/db_login.php');
+  $name = $db->real_escape_string($_GET['name']);
+  $address = $db->real_escape_string($_GET['address']);
+  $city = $db->real_escape_string($_GET['city']);
+
+  // assign query
+  $query = "INSERT INTO customers (name, address, city) VALUES('".$name."','"
+            .$address."','".$city."')";
+  $result = $db->query($query);
+  if (!$result) {
+    echo  '<div class="aler alert-danger alert-dismissible">
+            <strong>Error!</strong> Could not query the database <br>'
+            .$db->error
+            .'<br>query = '.$query
+          .'</div>';
+  } else {
+    echo  '<div class="alert alert-success alert-dismissible">
+            <strong>Success!</strong> Data has been added. <br>
+            Name: '.$name.'<br>
+            Address: '.$address.'<br>
+            City: '.$city.'<br>
+          </div>';
+  }
+
+  // close db connection
+  $db->close();
+?>
---
+++ 24060122140140/submission-5/view/get_book.php
@@ -0,0 +1,30 @@
+
+<?php
+require_once('./lib/db_login.php');
+
+$value = $_GET['title'];
+
+$query = "SELECT * FROM  books WHERE title LIKE '%".$value."%'";
+
+$result = $db->query($query);
+
+if (!$result) {
+    die("Could not query the database: <br>".$db->error);
+}
+
+$i = 1;
+while ($row = $result->fetch_object()) {
+    echo "<tr>";
+        echo "<td>".$i."</td>";
+        echo "<td>".$row->isbn."</td>";
+        echo "<td>".$row->author."</td>";
+        echo "<td>".$row->title."</td>";
+        echo "<td>".$row->price."</td>";
+    echo "</tr>";
+    $i++;
+}
+
+$result->free();
+$db->close();
+
+?>
