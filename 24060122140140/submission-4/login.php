004c79cbef611ddbb4c649af5746f68e3bf0c9a5
Aly <100826322+Falydra@users.noreply.github.com>
2024-10-13 14:41:41 +0700

    Update

--- 24060122140140/submission-4/login.php
+++ 24060122140140/submission-4/login.php
@@ -1,79 +1,78 @@
 <?php
-// TODO 1: Buat sebuah sesi baru
-session_start();
 
-// TODO 2 : Lakukan koneksi dengan database
-require_once('./lib/db_login.php');
 
-?>
-<?php include('./header.php') ?>
-<br>
-<div class="card mt-4">
-    <div class="card-header">Login Form</div>
-    <div class="card-body">
-        <form method="POST" autocomplete="on" action="<?= htmlspecialchars($_SERVER['PHP_SELF']) ?>">
-        <!-- // Memeriksa apakah user sudah submit form -->
-        <?php
-        if (isset($_POST["submit"])) {
-            $valid = TRUE; //flag validasi
 
-            // Memeriksa validasi email
-            $email = test_input($_POST['email']);
-            if ($email == '') {
-                $error_email = 'Email is required';
-                $valid = FALSE;
-            } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $error_email = 'Invalid email format';
-                $valid = FALSE;
-            }
+session_start(); //inisialisasi session
+require_once('lib/db_login.php');
 
-            // Memeriksa validasi password
-            $password = test_input($_POST['password']);
-            if ($password == '') {
-                $error_password = 'Password is required';
-                $valid = FALSE;
-            }
 
-            // Memeriksa validasi
-            if ($valid) {
-            
-                // TODO 3: Buatlah query untuk melakukan verifikasi terhadap kredensial yang diberikan
-                $query = "SELECT * FROM admin WHERE email='".$email."' AND password='".md5($password)."' "; 
-                
-                // TODO 4: Eksekusi query
-                $result = $db->query($query);
-                if(!$result){
-                    die("Could not query the database: <br/>".$db->error);
-                }
-                else{
-                    if($result->num_rows > 0){ //login berhasil
-                        $_SESSION['username']=$email;
-                        header('Location: admin.php');
-                        exit;
-                    }
-                    else{
-                        echo '<div class="error">Combination of username and password are not correct.</div>';
-                    }
-                }
-                // TODO 5: Tutup koneksi dengan database
-                $db->close();
+$email = '';
+$error_email = '';
+$error_password = '';
+
+//cek apakah user sudah submit form 
+if (isset($_POST["submit"])) {
+    $valid = TRUE; //flag validasi
+    //cek validasi email
+    $email = test_input($_POST['email']); 
+    if ($email == '') {
+        $error_email = "Email is required"; 
+        $valid = FALSE;
+    }elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+        $error_email = "Invalid email format";
+        $valid = FALSE;
+    }
+
+    //cek validasi 
+    $password = test_input ($_POST['password']);
+    if ($password== '') {
+        $error_password = "Password is required";
+        $valid = FALSE;
+    }
+
+    //cek validasi
+    if ($valid) {
+    //Asign a query
+        $query = "SELECT * FROM admin WHERE email='".$email."' AND password='".md5($password)."' ";
+        // Execute the query
+        $result = $db->query($query);
+        if (!$result) {
+            die ("Could not query the database: <br />". $db->error);
+        }else{
+            if ($result->num_rows > 0) { //login berhasil
+                $_SESSION['username'] = $email;
+                header('Location: view_customer.php');
+                exit;
+            } else{ //login gagal
+                echo '<span class="error">Combination of username and password are not correct.</span>';
             }
         }
-        ?>
-        <br>
-            <div class="form-group">
-                <label for="email">Email:</label>
-                <input type="email" class="form-control" id="email" name="email" value="<?php if (isset($email)) echo $email; ?>">
-                <div class="error"><?php if (isset($error_email)) echo $error_email ?></div>
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" class="form-control" id="password" name="password" value="">
-                <div class="error"><?php if (isset($error_password)) echo $error_password ?></div>
-            </div>
-            <br>
-            <button type="submit" class="btn btn-primary" name="submit" value="submit">Login</button>
-        </form>
+        //close db connection
+        $db->close();
+    }
+}
+
+?>
+<?php include('lib/header.html') ?>
+<br>
+<div class="card">
+    <div class="card-header">Login Form</div>
+        <div class="card-body">
+            <form method="POST" autocomplete="on" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" name="email" size="30" value ="<?php echo $email; ?>">
+                    <div class="error"><?php if (isset($error_email)) echo $error_email; ?></div> 
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" name="password" value= "">
+                    <div class="error"><?php if (isset($error_password)) echo $error_password; ?></div> 
+                </div>
+                <br>
+                <button type="submit" class="btn btn-primary" name="submit" value="submit">Login </button>
+            </form>
+        </div>
     </div>
 </div>
-<?php include('./footer.php') ?>
+<?php include('lib/footer.html') ?>
